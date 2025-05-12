7fd079dca138d8b5f3879fad60354b364c09ce6a
meraeugene <villalon.andrew123@gmail.com>
2025-05-12 23:30:39 +0800

    change to cloudinary image

--- waterbilling/urls.py
+++ waterbilling/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Redirect the homepage to the login page
+    path('', lambda request: redirect('/accounts/login/', permanent=False)),
     path('accounts/', include('allauth.urls')),
-    path('', include('core.urls')),
-    path('', include('pwa.urls')),  
-    
+    path('core/', include('core.urls')),  # Ensure core urls are prefixed to avoid conflict
+    path('pwa/', include('pwa.urls')),  # Same for pwa to avoid conflict
 ]
 
 # Media file serving in development
